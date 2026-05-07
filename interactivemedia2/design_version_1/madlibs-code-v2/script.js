f65767eb7a36c5353b73ea38f88e4451a5d72ee1
rkaneko-UCD <rkaneko@ucdavis.edu>
2026-05-07 05:52:56 -0700

    Update script.js

--- interactivemedia2/design_version_1/madlibs-code-v2/script.js
+++ interactivemedia2/design_version_1/madlibs-code-v2/script.js
@@ -2,11 +2,9 @@
     'use strict';
     console.log('Reading JS'); 
 
-    
     const myForm = document.querySelector('#madlibsForm');
     const storyOutput = document.querySelector('#storyOutput');
 
-   
     myForm.addEventListener('submit', function (event) {
         event.preventDefault(); 
 
@@ -20,49 +18,45 @@
 
         let myText; 
         
+      
         if (adj1 == '') {
-            myText = "Please provide an adjective";
+            myText = "<span class='error-msg'>Please provide an adjective</span>";
             document.querySelector('#adjective1').focus();
         } 
         else if (place == '') {
-            myText = "Please provide a place";
+            myText = "<span class='error-msg'>Please provide a place</span>";
             document.querySelector('#place').focus();
         } 
         else if (noun == '') {
-            myText = "Please provide a noun";
+            myText = "<span class='error-msg'>Please provide a noun</span>";
             document.querySelector('#noun').focus();
         } 
         else if (verb == '') {
-            myText = "Please provide a verb";
+            myText = "<span class='error-msg'>Please provide a verb</span>";
             document.querySelector('#verbIng').focus();
         } 
         else if (adj2 == '') {
-            myText = "Please provide another adjective";
+            myText = "<span class='error-msg'>Please provide another adjective</span>";
             document.querySelector('#adjective2').focus();
         } 
         else if (pluralNoun == '') {
-            myText = "Please provide a plural noun";
+            myText = "<span class='error-msg'>Please provide a plural noun</span>";
             document.querySelector('#pluralNoun').focus();
         } 
         else if (emotion == '') {
-            myText = "Please provide an emotion";
+            myText = "<span class='error-msg'>Please provide an emotion</span>";
             document.querySelector('#emotion').focus();
         } 
         
         else {
-           
-            myText = `On one lovely morning, I woke up feeling extremely <span class="user-word">${adj1}</span> and embarked on a journey to the enigmatic <span class="user-word">${place}</span>. Upon arrival, I was warmly welcomed by an amiable <span class="user-word">${noun}</span> who was <span class="user-word">${verb}</span> atop a humongous marshmallow. Everything appeared utterly <span class="user-word">${adj2}</span>! In a sudden flurry, showers of <span class="user-word">${pluralNoun}</span> rained down from above, shining in every hue of the spectrum. I was deeply moved by a strong sense of <span class="user-word">${emotion}</span>, realizing that anything was achievable in this surreal world.`;
+        
+            myText = `<h2>A Dream in Marshmallow Land</h2>
+            <p>On one lovely morning, I woke up feeling extremely <span class="user-word">${adj1}</span> and embarked on a journey to the enigmatic <span class="user-word">${place}</span>. Upon arrival, I was warmly welcomed by an amiable <span class="user-word">${noun}</span> who was <span class="user-word">${verb}</span> atop a humongous marshmallow. Everything appeared utterly <span class="user-word">${adj2}</span>! In a sudden flurry, showers of <span class="user-word">${pluralNoun}</span> rained down from above, shining in every hue of the spectrum. I was deeply moved by a strong sense of <span class="user-word">${emotion}</span>, realizing that anything was achievable in this surreal world.</p>`;
 
-            document.querySelector('#adjective1').value = '';
-            document.querySelector('#place').value = '';
-            document.querySelector('#noun').value = '';
-            document.querySelector('#verbIng').value = '';
-            document.querySelector('#adjective2').value = '';
-            document.querySelector('#pluralNoun').value = '';
-            document.querySelector('#emotion').value = '';
+       
+            myForm.reset();
         }
 
         storyOutput.innerHTML = myText;
-
     });
 })();
